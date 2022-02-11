04a82b324141366f0f55a31d77d1c957907ee9be
Kyoo130 <kml9003@kakao.com>
2022-02-12 03:31:22 +0900

    :sparkles: Components Add Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Toggle from "./component/Toggle";
+import Tag from "./component/Tag";
+import ClickToEdit from "./component/ClickToEdit";
+import Tab from "./component/Tab";
+import Modal from "./component/Modal";
 
 function App() {
   return (
     <div className="App">
-      <h1>wanted_pre_onboarding</h1>
+      <h1>원티드 프리온보딩</h1>
+      <Toggle />
+      <Modal />
+      <Tab />
+      <Tag />
+      <ClickToEdit />
     </div>
   );
 }
